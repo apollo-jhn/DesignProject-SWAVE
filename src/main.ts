e9d76d4b9f87e3190569acc83b4706a9148436be
apollo_jhn <japollo.ponteras@gmail.com>
2025-04-10 01:48:27 +0800

    electron update

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,15 @@ import {
   BrowserWindow,
   type BrowserWindowConstructorOptions,
 } from "electron";
+import { findPackageJSON } from "node:module";
 import path from "node:path";
 
 function createWindow(): void {
   const options: BrowserWindowConstructorOptions = {
+    // Width and Height are configured to match the size of the touchscreen panel
     width: 800,
-    height: 600,
+    height: 480,
+    useContentSize: true, // The size is required
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
@@ -16,6 +19,8 @@ function createWindow(): void {
 
   const win = new BrowserWindow(options);
 
+  // Disabling Menubar as it is not required
+  win.setMenuBarVisibility(false)
   win.loadFile("index.html");
 }
 
