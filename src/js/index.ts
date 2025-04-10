5179d19a00404223d7a14e320d93b846494e4fd3
apollo_jhn <japollo.ponteras@gmail.com>
2025-04-10 12:30:52 +0800

    navigation bar

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,6 +1,8 @@
 // After HTML has been loaded
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Welcome to SWAVE!")
+  console.log("Welcome to SWAVE!");
 });
 
-
+const app = document.getElementById("app") as HTMLDivElement | null
+const header_title = document.getElementById("header_title") as HTMLHeadingElement | null
+const websocket_status = document.getElementById("websocket_status") as HTMLSpanElement | null
